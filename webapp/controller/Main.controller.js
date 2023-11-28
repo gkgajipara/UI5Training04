5ee07c517242c91da82e55694a863c75a7c5c10f
bas04 clouddnagmbhgmail.onmicrosoft.com <bas04@clouddnagmbhgmail.onmicrosoft.com>
2023-11-28 11:22:16 +0000

    Kapitel 5 Ende

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,5 +7,19 @@ sap.ui.define([
       onInit: function() {
         
       },
+
+      genderFormatter: function (sKey) {
+        let oView = this.getView();
+        let oI18nModel = oView.getModel("i18n");
+        let oResourceBundle = oI18nModel.getResourceBundle();
+        let sText = oResourceBundle.getText(sKey);
+        return sText;
+    },
+    
+    dateFormatter: function(date) {
+        let dateObj = new Date(date);
+        return dateObj.getDate() + "." + (dateObj.getMonth() + 1) + "." + dateObj.getFullYear();
+    }
+    
     });
   });
